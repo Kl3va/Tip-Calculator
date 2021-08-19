a92c167b0af1cf06ed247943a96a9b91dd3bb8f4
Xurd3 <61953663+Xurd3@users.noreply.github.com>
2021-08-19 09:02:59 +0100

    A great test on JavaScript
    Tip-Calculator

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# Tip Calculator
- A junior level frontendmentor.io project
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+My users will be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Desktop-first workflow
+- Vanilla JavaScript
+
+### What I learned
+
+I must say I learnt a lot in this project especially working with eventListeners. The codes below were a thing of joy for me after i got them working. I enjoyed the logic application for the tip-calculator. Also, google was my friend through out the entire process especially hiding the arrows within the textfield in css.
+
+see below:
+
+```html
+<input
+  type="number"
+  name="number"
+  class="bills bills--2"
+  placeholder="0"
+  oninput="checkLength(2,this)"
+  onblur="invalid(this)"
+  onfocus="valid(this)"
+/>
+```
+
+```css
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+```
+
+```js
+if (billValue > 0 && persons > 0 && persons <= 20 && e.target.value > 0) {
+  const tipAmount = (billValue / persons) * Number(e.target.value / 100);
+  tipValue.textContent = `$${tipAmount.toFixed(2)}`;
+  const totalAmount = billValue / persons + tipAmount;
+  totalValue.textContent = `$${totalAmount.toFixed(2)}`;
+} else {
+  totalValue.textContent = tipValue.textContent = "$0.00";
+}
+```
+
+### Continued development
+
+Still getting pretty confident writing frontend code. I must say I'm impressed with my progress so far. More focus will be on DOM manipulation.
+
+## Author
+
+- Frontend Mentor - [@Kl3va](https://www.frontendmentor.io/profile/Kl3va)
+- Twitter - [@_Kl3va_](https://www.twitter.com/_Kl3va_)
+
+## Acknowledgments
+
+I would like to give a shout out to Jonas Schmedtmann. His udemy courses have been very helpful in my journey to become a professional FrontEnd Developer and also to the entire team at frontendmentor... Much love.
---
+++ tip-calculator-app-main/script.js
@@ -0,0 +1,96 @@
+'use script';
+//Selected Elements
+const bill = document.querySelector('.bills--1');
+const numberOfPeople = document.querySelector('.bills--2');
+const btns = document.querySelector('.btns');
+const customBtn = document.querySelector('.custom');
+const errorMessage = document.querySelector('.error-message');
+const tipValue = document.querySelector('.tip-value');
+const totalValue = document.querySelector('.total-value');
+const btnReset = document.querySelector('.btn-reset');
+const input = document.querySelectorAll('input[type=number]');
+//console.log(input);
+
+//Restricting Character length
+function checkLength(len, ele) {
+  let fieldLength = ele.value.length;
+  if (fieldLength <= len) {
+    return true;
+  } else {
+    let str = ele.value;
+    str = str.substring(0, str.length - 1);
+    ele.value = str;
+  }
+}
+
+//onBlur Event
+function invalid(ele) {
+  if (Number(ele.value) <= 0) {
+    ele.style.outline = '2px solid red';
+  } else {
+    ele.style.outline = '';
+  }
+}
+
+//onFocus Event
+function valid(ele) {
+  if (Number(ele.value) >= 0) {
+    ele.style.outline = '2px solid hsl(172, 67%, 45%)';
+  } else {
+    ele.style.outline = '';
+  }
+}
+
+//Accessing the buttons using DOM delegation and performing the calculation
+btns.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    e.target.classList.contains('btn') ||
+    e.target.classList.contains('custom')
+  ) {
+    //console.log(e.target);
+
+    const billValue = Number(bill.value);
+    const persons = Number(numberOfPeople.value);
+    //console.log(billValue);
+
+    //Tip calculation
+
+    if (persons <= 0 || persons > 20) {
+      // numberOfPeople.classList.add('bills-color');
+      // invalid(numberOfPeople);
+      errorMessage.textContent = `Can't be ${
+        persons <= 0 ? persons : `more than 20`
+      }`;
+    } else {
+      errorMessage.textContent = '';
+      // numberOfPeople.classList.remove('bills-color');
+    }
+
+    if (billValue > 0 && persons > 0 && persons <= 20 && e.target.value > 0) {
+      const tipAmount = (billValue / persons) * Number(e.target.value / 100);
+      tipValue.textContent = `$${tipAmount.toFixed(2)}`;
+      const totalAmount = billValue / persons + tipAmount;
+      totalValue.textContent = `$${totalAmount.toFixed(2)}`;
+    } else {
+      totalValue.textContent = tipValue.textContent = '$0.00';
+    }
+  }
+});
+
+//Reset Button
+btnReset.addEventListener('click', function (e) {
+  e.preventDefault();
+  tipValue.textContent = '$0.00';
+  totalValue.textContent = '$0.00';
+  errorMessage.textContent = '';
+  tipAmount = 0;
+  totalAmount = 0;
+  bill.value = '';
+  numberOfPeople.value = '';
+  customBtn.value = '';
+  input.forEach(function (nodelist) {
+    return (nodelist.style.outline = '');
+  });
+});
